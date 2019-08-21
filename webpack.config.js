ba9a83f69d1efc2baa4ddd91172ea5e75836b8f1
ClayGao <r700000@gmail.com>
2019-08-21 17:16:30 +0800

    Install sass-loader css-loader style-loader

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // 引入的檔案
+  output: {
+    path: path.resolve(__dirname, 'dist'), // 輸出的位置
+    filename: 'index.bundle.js' // * html 要引入的檔案
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  }
+};
