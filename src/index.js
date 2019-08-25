1e87a94f522d03198ef852348232c2af27f71978
ClayGao <r700000@gmail.com>
2019-08-25 18:18:32 +0800

    todolost ver 1.0 ok

--- src/index.js
+++ src/index.js
@@ -1,65 +1,71 @@
 import './style.scss';
 import $ from 'jquery';
 import 'jquery-ui-bundle';
+import  { render, runModelAjax } from'./utils'
 
 $(document).ready(()=>{
+
     let isChecked = false;
-
-    function addTodoTasks(arr){
-        for(let i=0; i < arr.length; i+=1){
-            const todoEvent = `<div class="todo-event" id="${arr[i]['todoID']}">
-                                    <div class="todo-event-title">
-                                        ${arr[i]['todoTitle']}
-                                    </div>
-                                    <div class="todo-event-context">
-                                        ${arr[i]['todoComment']}
-                                    </div>
-                                </div>`
-            $('.todo').append(todoEvent)
-        }
-    }
-
-    function render(){
-        $.ajax({
-            type: 'GET',
-            url: './models/get_comments.php',
-            success(resp){
-                $('.todo').empty()
-                const json = JSON.parse(resp)
-                addTodoTasks(json)
-            }
-        })
-    }
+    let isUpdating = false;
+    let currentTaskID = 0;
 
     $('.key-in').click((e)=>{
         if(e.target.type === 'radio') {
             const textColor = e.target.value
             $('.key-in-level').val(textColor)
-            $('.key-in-block > textarea').animate({'background-color':textColor})
+            console.log(textColor)
+            $('.key-in-block > input').animate({'background-color':textColor})
             isChecked = true;
         }
-        if(e.target.type === 'button' && isChecked) {
-            const todoTitle = $('.key-in-block > input').val()
-            const todoContext = $('.key-in-block > textarea').val()
-            const todoLevel =  $('.key-in-level').val()
-            if(!todoTitle || !todoContext) return
-            $.ajax({
-                type: 'POST',
-                url: './models/add_comments.php',
-                data: {
-                    todoTitle : todoTitle,
-                    todoContext : todoContext,
-                    todoLevel : todoLevel
-                },
-                success(){
-                    console.log('OK')
-                    $('.key-in-block > input').val('')
-                    $('.key-in-block > textarea').val('')
-                    render()
-                }
-            })
+        if (e.target.type === 'button' && isChecked && isUpdating) {
+            const updateData = {
+                updateTitle : $('.key-in-block > input').val(),
+                updateContext : $('.key-in-block > textarea').val(),
+                updateLevel :  $('.key-in-level').val(),
+                updateID : currentTaskID
+            }
+
+            if(!updateData.updateTitle || !updateData.updateContext) return
+            runModelAjax('POST','./models/todo_API.php',updateData)
+            console.log('update')
+        } else if(e.target.type === 'button' && isChecked) {
+            const data = {
+                todoTitle : $('.key-in-block > input').val(),
+                todoContext : $('.key-in-block > textarea').val(),
+                todoLevel :  $('.key-in-level').val()
+            }
+
+            if(!data.todoTitle || !data.todoContext) return
+            runModelAjax('POST','./models/todo_API.php',data)
         }
     })
 
+    $('.todo').click((e)=>{
+        const targetTaskID = e.target.parentElement.id
+        const targetTask = $(`.todo-event[id='${targetTaskID}']`)
+        const targetTaskTitle = $(`.todo-event[id='${targetTaskID}'] > .todo-event-title`).text().trim()
+        const targetTaskContext = $(`.todo-event[id='${targetTaskID}'] > .todo-event-context`).text().trim()
+        if(!targetTaskID) return
+        $(`.todo-event-options[id=${targetTaskID}]`)
+        .slideToggle()
+        .click((evt)=>{
+            if (evt.target.type !== 'button') return
+            const option = evt.target.value
+            if( option === 'C') {
+                runModelAjax('PATCH',`./models/todo_API.php?taskid=${targetTaskID}`)
+            } else if ( option === 'U') {
+                targetTask.hide()
+                $('.key-in-block > input').val(`Editing -> ${targetTaskTitle}`)
+                $('.key-in-block > textarea').val(targetTaskContext)
+                $('.todo-edit-id').val(targetTaskID)
+                isUpdating = true;
+                currentTaskID = targetTaskID;
+                //runModelAjax('POST',`./models/update_comments.php`)
+            } else {
+                runModelAjax('DELETE',`./models/todo_API.php?taskid=${targetTaskID}`)
+            }
+        })
+    })
+
     render()
-})
+}) 
